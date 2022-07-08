0c72d24b14276372feb94535e4a3510256762aef
txjxs <tejasnisar2000@gmail.com>
2022-07-08 23:32:56 +0530

    css card for recommends done

--- index.js
+++ index.js
@@ -68,6 +68,22 @@ const createSlide = () => {
 for (let i = 0; i < 3; i++) {
 	createSlide();
 }
-setInterval(() => {
-	createSlide();
-}, 3000);
+// setInterval(() => {
+// 	createSlide();
+// }, 3000);
+
+
+
+const videoCards = [...document.querySelectorAll('.video-card')];
+videoCards.forEach(item => {
+	item.addEventListener('mouseover', ()=>{
+		let video = item.children[1];
+		video.play();
+
+	})
+	item.addEventListener('mouseleave', ()=>{
+		let video = item.children[1];
+		video.pause();
+		
+	})
+})
