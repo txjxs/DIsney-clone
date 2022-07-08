798b6101444f8578bb446ef173180a7a9836ea5d
txjxs <tejasnisar2000@gmail.com>
2022-07-08 21:09:35 +0530

    done till carousel

---
+++ index.js
@@ -0,0 +1,73 @@
+let movies = [
+	{
+		name: 'falcon and the winter soldier',
+		des:
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,.",
+		image: 'Images/slider 2.PNG'
+	},
+	{
+		name: 'loki',
+		des:
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,.",
+		image: 'Images/slider 1.PNG'
+	},
+	{
+		name: 'wanda vision',
+		des:
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,.",
+		image: 'Images/slider 3.PNG'
+	},
+	{
+		name: 'raya and the last dragon',
+		des:
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,.",
+		image: 'Images/slider 4.PNG'
+	},
+	{
+		name: 'luca',
+		des:
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,.",
+		image: 'Images/slider 5.PNG'
+	}
+];
+
+const carousel = document.querySelector('.carousel');
+let sliders = [];
+let slideIndex = 0; //track currunt silde
+const createSlide = () => {
+	if (slideIndex >= movies.length) {
+		slideIndex = 0;
+	}
+	let slide = document.createElement('div');
+	var imgElement = document.createElement('img');
+	let content = document.createElement('div');
+	let h1 = document.createElement('h1');
+	let p = document.createElement('p');
+
+	imgElement.appendChild(document.createTextNode(''));
+	h1.appendChild(document.createTextNode(movies[slideIndex].name));
+	p.appendChild(document.createTextNode(movies[slideIndex].des));
+	content.appendChild(h1);
+	content.appendChild(p);
+	slide.appendChild(content);
+	slide.appendChild(imgElement);
+	carousel.appendChild(slide);
+
+	imgElement.src = movies[slideIndex].image;
+	slideIndex++;
+	slide.className = 'slider';
+	content.className = 'slide-content';
+	h1.className = 'movie-title';
+	p.className = 'movie-des';
+
+	sliders.push(slide);
+	if (sliders.length) {
+		sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${30 * (sliders.length - 2)}px)`;
+	}
+};
+for (let i = 0; i < 3; i++) {
+	createSlide();
+}
+setInterval(() => {
+	createSlide();
+}, 3000);
